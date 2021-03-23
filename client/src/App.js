1a67fae624ae431b3cf1622c70ce1803041cadd9
ChrisB007 <chrisbitoy@gmail.com>
2021-03-23 15:21:59 -0400

    routes update

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,44 @@ import React from 'react';
 import JumboText from './components/JumboText/JumboText';
 import Body from './components/Body/Body';
 import HeaderNavbar from './components/Header/Navbar/HeaderNav';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+import UserAuth from './components/Authentication/UserAuth';
+import Jobs from './components/Body/Jobs';
+import Questions from './components/Body/Questions';
+
 
 
 
 function App() {
   return (
-    <>
-    <HeaderNavbar />
-    <JumboText />
-    <Body />
-    </>
+    <Router>
+      <>
+        <Route path="/" exact={true}>
+          <HeaderNavbar />
+          <JumboText />
+          <Body />
+        </Route>
+
+        <Route path="/questions">
+          <HeaderNavbar />
+          <JumboText />
+          <Questions />
+        </Route>
+
+        <Route path="/jobs">
+          <HeaderNavbar />
+          <JumboText />
+          <Jobs />
+        </Route>
+
+        <Route path="/signup">
+          <HeaderNavbar />
+          <JumboText />
+          <UserAuth />
+        </Route>
+        </>
+    </Router>
   )
 }
 
